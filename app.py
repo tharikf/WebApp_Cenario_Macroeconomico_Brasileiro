dc4deec095f470f3bb8a874409f98a09fee43a43
Tharik <104634357+tharikf@users.noreply.github.com>
2022-11-11 19:15:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
-                meta_tags=[{"name": "viewport", "content": "width=device-width"}],
+                meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
                 suppress_callback_exceptions=True)
 
 server = app.server
