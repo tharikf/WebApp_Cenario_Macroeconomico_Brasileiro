7976859b205e758556f10b8dadd4b1147443a31e
Tharik <104634357+tharikf@users.noreply.github.com>
2022-11-11 17:38:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ app = dash.Dash(__name__,
                 meta_tags=[{"name": "viewport", "content": "width=device-width"}],
                 suppress_callback_exceptions=True)
 
+server = app.server
