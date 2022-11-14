eef48ebbcb944678a1cd8d221ca8a9252148b26c
Tharik <104634357+tharikf@users.noreply.github.com>
2022-11-14 20:19:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
                 meta_tags = [{'name': 'viewport', "content" : "width=device-width, initial-scale=0.19, maximum-scale=5, minimum-scale=0.1, device-height=50"}],
                 suppress_callback_exceptions=True)
+
+server = app.server
                 
